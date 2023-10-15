9a152667547ed471fe9da1108b5164f25e25767e
Light <47911535+LightAPIs@users.noreply.github.com>
2023-10-15 16:38:22 +0800

    feat: Add type

---
+++ README.md
@@ -0,0 +1,32 @@
+# arrive-type
+
+A generic version of `@types/arrive`.
+
+## Installation
+
+```shell
+npm install arrive
+npm install @dragonish/arrive-type -D
+```
+
+## Usage
+
+Add to the `tsconfig.json` file:
+
+{
+	"compilerOptions": {
+		"typeRoots": [
+			"./node_modules/@types",
+			"./node_modules/@dragonish/arrive-type"
+		]
+	}
+}
+
+## Credits
+
+- [DefinitelyTyped/DefinitelyTyped](https://github.com/DefinitelyTyped/DefinitelyTyped)
+- [uzairfarooq/arrive](https://github.com/uzairfarooq/arrive)
+
+## License
+
+[MIT](./LICENSE)
---
+++ src/index.d.ts
@@ -0,0 +1,83 @@
+// Type definitions for arrive 2.4
+// Project: https://github.com/uzairfarooq/arrive
+// Definitions by: Vijay Pemmaraju <https://github.com/vijaypemmaraju>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Modified by: Light <https://github.com/LightAPIs>
+
+// required for declare global to work
+export {};
+
+export interface Options {
+  fireOnAttributesModification?: boolean | undefined;
+  onceOnly?: boolean | undefined;
+  existing?: boolean | undefined;
+}
+
+export type ArriveSignature = <E extends Element = Element>(
+  element: string,
+  handlerOrOptions: ((this: E, element: E) => void) | Options,
+  handler?: (this: E, element: E) => void
+) => void;
+export type UnbindArriveSignature = <E extends Element = Element>(
+  elementOrHandler?: string | ((this: E, element: E) => void),
+  handler?: (this: E, element: E) => void
+) => void;
+export type LeaveSignature = <E extends Element = Element>(
+  element: string,
+  handlerOrOptions: ((this: E, element: E) => void) | Options,
+  handler?: (this: E) => void
+) => void;
+export type UnbindLeaveSignature = <E extends Element = Element>(
+  elementOrHandler?: string | ((this: E, element: E) => void),
+  handler?: (this: E, element: E) => void
+) => void;
+
+declare global {
+  // tslint:disable-next-line no-unnecessary-class
+  class Arrive {
+    static unbindAllArrive: () => void;
+    static unbindAllLeave: () => void;
+  }
+
+  interface Document {
+    arrive: ArriveSignature;
+    unbindArrive: UnbindArriveSignature;
+    leave: LeaveSignature;
+    unbindLeave: UnbindLeaveSignature;
+  }
+
+  interface JQuery {
+    arrive: ArriveSignature;
+    unbindArrive: UnbindArriveSignature;
+    leave: LeaveSignature;
+    unbindLeave: UnbindLeaveSignature;
+  }
+
+  interface Window {
+    arrive: ArriveSignature;
+    unbindArrive: UnbindArriveSignature;
+    leave: LeaveSignature;
+    unbindLeave: UnbindLeaveSignature;
+  }
+
+  interface NodeList {
+    arrive: ArriveSignature;
+    unbindArrive: UnbindArriveSignature;
+    leave: LeaveSignature;
+    unbindLeave: UnbindLeaveSignature;
+  }
+
+  interface Element {
+    arrive: ArriveSignature;
+    unbindArrive: UnbindArriveSignature;
+    leave: LeaveSignature;
+    unbindLeave: UnbindLeaveSignature;
+  }
+
+  interface HTMLCollectionBase {
+    arrive: ArriveSignature;
+    unbindArrive: UnbindArriveSignature;
+    leave: LeaveSignature;
+    unbindLeave: UnbindLeaveSignature;
+  }
+}
